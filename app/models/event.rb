5fc2ebf7d5e7e28de09e3163579bc45f04b811dd
maxmuzone <maxmuzone@gmail.com>
2019-06-16 10:53:17 -0400

    Events bug fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,10 +3,9 @@ class Event < ApplicationRecord
   belongs_to :user
 
   has_many :comments
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :delete_all
   has_many :subscribers, through: :subscriptions, source: :user
   has_many :photos
-  has_many :subscriptions, dependent: :delete_all
 
   validates :user, presence: true
   validates :title, presence: true, length: {maximum: 255}
